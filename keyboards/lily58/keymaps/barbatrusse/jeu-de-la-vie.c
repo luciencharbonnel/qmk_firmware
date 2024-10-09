0f3aac2fec8ac2ff4e60c5c1234dffe8d0ff97f3
Lucien Charbonnel <lucien_char@hotmail.com>
2024-10-09 22:50:28 +0200

    add fcts

--- keyboards/lily58/keymaps/barbatrusse/jeu-de-la-vie.c
+++ keyboards/lily58/keymaps/barbatrusse/jeu-de-la-vie.c
@@ -16,6 +16,27 @@ void print_grid(void) {
     }
 }
 
+uint8_t get_cell(int16_t x, int16_t y) {
+    while (x < 0)
+        x += WIDTH;
+    while (x >= WIDTH)
+        x -= WIDTH;
+    while (y < 0)
+        y += HEIGHT;
+    while (y >= HEIGHT)
+        y -= HEIGHT;
+
+    uint32_t* cell_ptr = grid + y;
+
+    return ((*(cell_ptr) >> x) & 0x01) ? 1 : 0;
+}
+
+void cell_on(int8_t x, int8_t y) {}
+
+void update_grid() {
+    uint8_t neighbors_count;
+}
+
 void jeu(uint16_t k) {
     print_grid();
 }
